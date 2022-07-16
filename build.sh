9ffdeab6ff8afa43cbdada4563c2a7e9d54423fd
Holger Hans Peter Freyther <holger.freyther@gs.com>
2022-07-16 11:21:08 +0800

    oss: Create a source tarball, build and push container
    Create a dist target that builds a tarball, builds from the tarball
    and places it inside the docker container. The final contasiner will
    be pushed to a registry. We need to dynamically adjust the name of
    the registry.

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Builds the container image from the source tarball.
+
+set -e
+set -o pipefail
+
+INPUT="$(realpath $1)"
+COMMIT="$2"
+
+echo "Attempting to build ${INPUT}"
+
+mkdir build_dir
+tar -C build_dir -xf "${INPUT}"
+
+# My patch failed with out of memory. Let's use git instead.
+cd build_dir/grafana-dist
+git init
+git config user.email "you@example.com"
+git config user.name "Your Name"
+git add *
+git commit -m "baseline version"
+git am patches/*.patch
+
+
+mkdir -p public/build/static
+cp "${INPUT}" public/build/static
+docker build --tag ghcr.io/goldmansachs/grafana:9-ubi8-"${COMMIT}" -f ./Dockerfile.ubi8 .
